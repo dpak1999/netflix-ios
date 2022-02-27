92291a8ef92b9a045f181c0e8deb583d22c79a56
deepak <deepakkumardash313@gmail.com>
2022-02-28 00:15:24 +0530

    Initial Commit

---
+++ netlfix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netlfix-clone
+//
+//  Created by Deepak kumar Dash on 28/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
